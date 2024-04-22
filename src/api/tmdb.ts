c03c17ca4fad12f80c5dffdcebd407e78ffd8226
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2024-04-22 23:18:07 +0300

    6. Custom hooks and memory leak

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,55 +1,65 @@
 import configuration from "../configuration";
 
-const apiBasePath = `${configuration.apiUrl}/3`
+const apiBasePath = `${configuration.apiUrl}/3`;
 
 async function get<TBody>(relativeUrl: string): Promise<TBody> {
-    var headers = new Headers();
-    headers.append("Accept", "application/json");
-    headers.append("Authorization", `Bearer ${configuration.apiToken}`);
+  var headers = new Headers();
+  headers.append("Accept", "application/json");
+  headers.append("Authorization", `Bearer ${configuration.apiToken}`);
 
-    var requestOptions = {
-        method: "GET",
-        headers: headers
-    };
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+  };
 
-    const response = await fetch(`${apiBasePath}${relativeUrl}`, requestOptions);
-    const value: TBody = await response.json();
-    return value;
+  const response = await fetch(`${apiBasePath}${relativeUrl}`, requestOptions);
+  const value: TBody = await response.json();
+  return value;
 }
 
 interface PageResponse<TResult> {
-    page: number;
-    results: TResult[],
-    total_pages: number;
-    total_results: number;
+  page: number;
+  results: TResult[];
+  total_pages: number;
+  total_results: number;
 }
 
 interface MovieDetails {
-    id: number;
-    title: string;
-    overview: string;
-    popularity: number;
-    backdrop_path?: string | null;
+  id: number;
+  title: string;
+  overview: string;
+  popularity: number;
+  backdrop_path?: string | null;
+}
+
+interface PageDetails<T> {
+  results: T[];
+  page: number;
+  totalPages: number;
 }
 
 interface Configuration {
-    images: {
-        base_url: string;
-    }
+  images: {
+    base_url: string;
+  };
 }
 
 interface ITmbdClient {
-    getConfiguration: () => Promise<Configuration>;
-    getNowPlaying: () => Promise<MovieDetails[]>;
+  getConfiguration: () => Promise<Configuration>;
+  getNowPlaying: (page: number) => Promise<PageDetails<MovieDetails>>;
 }
 
 export const client: ITmbdClient = {
-    getConfiguration: async () => {
-        const response = await get<Configuration>("/configuration");
-        return response;
-    },
-    getNowPlaying: async () => {
-        const response = await get<PageResponse<MovieDetails>>("/movie/now_playing");
-        return response.results;
-    }
-}
+  getConfiguration: async () => {
+    const response = await get<Configuration>("/configuration");
+    return response;
+  },
+  getNowPlaying: async (page: number = 1) => {
+    const response = await get<PageResponse<MovieDetails>>(`/movie/now_playing?page=${page}`);
+    return {
+      results: response.results,
+      totalPages: response.total_pages,
+      page: response.page,
+    };
+  },
+};
--- src/features/Movies/moviesSlice.ts
+++ src/features/Movies/moviesSlice.ts
@@ -3,66 +3,82 @@ import { ActionWithPayload, createReducer } from "../../redux/utils";
 import { AppThunk } from "../../store";
 
 export interface Movie {
-    id: number;
-    title: string;
-    overview: string;
-    popularity: number;
-    image?: string;
+  id: number;
+  title: string;
+  overview: string;
+  popularity: number;
+  image?: string;
 }
 
 interface MoviesState {
-    loading: boolean;
-    top: Movie[]
+  loading: boolean;
+  top: Movie[];
+  page: number;
+  hasMorePages: boolean;
 }
 
 const initialState: MoviesState = {
-    loading: false,
-    top: []
-}
+  loading: false,
+  top: [],
+  page: 0,
+  hasMorePages: true,
+};
 
 function loading() {
+  return {
+    type: "movies/loading",
+  };
+}
+
+function loaded(movies: Movie[], page: number, hasMorePages: boolean) {
+  return {
+    type: "movies/loaded",
+    payload: { movies, page, hasMorePages },
+  };
+}
+
+export function fetchNextPage(): AppThunk<Promise<void>> {
+  return async (dispatch, getState) => {
+    const state = getState();
+    const nextPage = state.movies.page + 1;
+    dispatch(fetchPage(nextPage));
+  };
+}
+
+function fetchPage(page: number): AppThunk<Promise<void>> {
+  return async (dispatch) => {
+    dispatch(loading());
+
+    const configuration = await client.getConfiguration(); // todo: single load per app
+    const nowPlaying = await client.getNowPlaying(page);
+    const imageSize = "w780";
+    const movies: Movie[] = nowPlaying.results.map((movie) => ({
+      id: movie.id,
+      title: movie.title,
+      overview: movie.overview,
+      popularity: movie.popularity,
+      image: movie.backdrop_path ? `${configuration.images.base_url}${imageSize}${movie.backdrop_path}` : undefined,
+    }));
+
+    const hasMorePages = nowPlaying.page < nowPlaying.totalPages;
+
+    dispatch(loaded(movies, page, hasMorePages));
+  };
+}
+
+const moviesReducer = createReducer<MoviesState>(initialState, {
+  "movies/loading": (state) => {
+    return { ...state, loading: true };
+  },
+  "movies/loaded": (state, action: ActionWithPayload<{ movies: Movie[]; page: number; hasMorePages: boolean }>) => {
     return {
-        type: "movies/loading"
+      ...state,
+      top: [...state.top, ...action.payload.movies],
+      page: action.payload.page,
+      hasMorePages: action.payload.hasMorePages,
+      loading: false,
     };
-}
-
-function loaded(movies: Movie[]) {
-    return {
-        type: "movies/loaded",
-        payload: movies
-    };
-}
-
-// export type AppThunk<ReturnType> = ThunkAction<ReturnType, MoviesState, undefined, UnknownAction>;
-
-export function fetchMovies(): AppThunk<Promise<void>> {
-    return async (dispatch, getState) => {
-        dispatch(loading());
-
-        const configuration = await client.getConfiguration(); // todo: single load per app
-        const results = await client.getNowPlaying();
-        const imageSize = "w780";
-        const movies: Movie[] = results.map((movie) => ({
-            id: movie.id,
-            title: movie.title,
-            overview: movie.overview,
-            popularity: movie.popularity,
-            image: movie.backdrop_path ? `${configuration.images.base_url}${imageSize}${movie.backdrop_path}` : undefined
-        }));
-
-        dispatch(loaded(movies))
-    }
-}
-
-const moviesReducer = createReducer<MoviesState>(
-    initialState,
-    {
-        "movies/loading": (state, action: ActionWithPayload<boolean>) => {
-            return { ...state, loading: true };
-        },
-        "movies/loaded": (state, action: ActionWithPayload<Movie[]>) => {
-            return { ...state, top: action.payload, loading: false };
-        },
-    });
+  },
+});
 
 export default moviesReducer;
